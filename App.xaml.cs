ffef4998551cc3b32d2dc2bee6a2170ab41f82f6
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-10-03 20:50:43 +0200

    got App Center working for Android with crashes and analytics

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace MauiAppCenter;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace MauiAppCenter;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
+
+		AppCenter.Start("android=\"c86d8086-5d05-4934-92ec-983860f15d13\"", typeof(Analytics), typeof(Crashes));
 
 		MainPage = new AppShell();
 	}
